225a9e893c7457e5092589aebe52f24ca2901615
shane.neff@outlook.com <shane.neff@outlook.com>
2025-05-05 14:08:08 -0400

    updated main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name   = "rg-cummins-tf-lab"
     storage_account_name  = "stgsnefftflab"
@@ -9,9 +15,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  client_id       = var.AZURE_CLIENT_ID
-  client_secret   = var.AZURE_CLIENT_SECRET
-  subscription_id = var.AZURE_SUBSCRIPTION_ID
-  tenant_id       = var.AZURE_TENANT_ID
 }
+
