c3b8eed4077c0c5902e4c8c0a7afb31607686860
shane.neff@outlook.com <shane.neff@outlook.com>
2025-05-05 15:27:05 -0400

    creds

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -10,6 +10,7 @@ terraform {
     storage_account_name  = "stgsnefftflab"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
+    use_azuread_auth      = true
   }
 }
 
