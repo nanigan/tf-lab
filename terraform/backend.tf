6e5994c6f7eee89dbd2e0f755c49226aae7213b4
shane.neff@outlook.com <shane.neff@outlook.com>
2025-05-05 13:46:50 -0400

    update backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,12 +6,3 @@ terraform {
     key                   = "terraform.tfstate"
   }
 }
-
-provider "azurerm" {
-  features {}
-
-  client_id       = var.AZURE_CLIENT_ID
-  client_secret   = var.AZURE_CLIENT_SECRET
-  subscription_id = var.AZURE_SUBSCRIPTION_ID
-  tenant_id       = var.AZURE_TENANT_ID
-}
