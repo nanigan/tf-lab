3cc7443a8acfd8fa6942318bbca53c1d921f7a73
shane.neff@outlook.com <shane.neff@outlook.com>
2025-05-06 09:29:38 -0400

    yaml updated

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "rg-cummins-tf-lab"
-    storage_account_name  = "stgsnefftflab"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-    use_azuread_auth      = true
-  }
-}
