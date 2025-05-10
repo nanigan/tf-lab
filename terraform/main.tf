d7ffcc9fd797cb45c7220de3332f9d773da00632
Shane Neff <shane.neff@outlook.comm>
2025-05-10 15:35:47 -0400

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,24 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name   = "rg-cummins-tf-lab"
-    storage_account_name  = "stgsnefftflab"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-    use_azuread_auth      = true
-  }
-}
-
+# Configure the Azure Provider
 provider "azurerm" {
   features {}
-  
-client_id       = var.client_id
-client_secret   = var.client_secret
-subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-
 }
 
+# Configure Authentication (Using Federated Credentials)
+provider "azurerm" {
+  features {}
+
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+}
+
+# Define the Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Define Outputs (For Reference in GitHub Actions)
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,49 @@
-variable "rg-cummins-tf-lab" {}
-variable "westus2" {}
+# Azure Authentication Variables
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  default     = "742d51bd-901c-4bbe-baff-fc05a7073fd2"
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  default     = "1f4db9dc-679c-4e26-bfe5-e7ceda3c87e3"
+}
+
+variable "client_id" {
+  description = "Azure Client ID for authentication"
+  type        = string
+}
+
+# Terraform Backend Storage Variables
+variable "storage_account_name" {
+  description = "The name of the Azure Storage Account for Terraform state"
+  type        = string
+  default     = "stgsnefftflab"
+}
+
+variable "container_name" {
+  description = "The name of the Blob Storage container for Terraform state"
+  type        = string
+  default     = "tfstate"
+}
+
+variable "resource_group_name" {
+  description = "The name of the Resource Group"
+  type        = string
+  default     = "westus2"
+}
+
+variable "key" {
+  description = "The name of the Terraform state file"
+  type        = string
+  default     = "terraform.tfstate"
+}
+
+# Resource Group Location
+variable "location" {
+  description = "Azure region where resources will be deployed"
+  type        = string
+  default     = "westus2"
+}
