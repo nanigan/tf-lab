5208196ed767f2cc3c3d601474fc35062da4eee4
shane.neff@outlook.com <shane.neff@outlook.com>
2025-05-21 18:26:02 -0400

    update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "rg-cummins-tf-lab2-wh701"
-    storage_account_name  = "sa01poc20099wh701"
+    resource_group_name   = "rg-cummins-tf-lab"
+    storage_account_name  = "stgsnefftflab"
     container_name        = "tfstate"
     key                   = "PLACEHOLDER"  # Will be overridden via CLI
     use_oidc              = true
